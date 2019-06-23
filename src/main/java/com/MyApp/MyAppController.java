6d62dd145b00ec63a6f61f174c79d3ec15a868db
Vinay Pandey <vinay.pandey12@gmail.com>
2019-06-24 00:18:25 +0530

    class request mapping

--- src/main/java/com/MyApp/MyAppController.java
+++ src/main/java/com/MyApp/MyAppController.java
@@ -11,32 +11,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/example/v1/users")
 public class MyAppController {
 
 	@RequestMapping("/welcome")
 	public String WelcomeMessage() {
 		return "Welcome to Spring Boot App";
 	}
-
-//	@RequestMapping(value = "/users", method = RequestMethod.GET)
-//	public String Users() {
-//		return "List all the users";
-//	}
-//
-//	@RequestMapping(value = "/users/add", method = RequestMethod.POST)
-//	public String AddUser() {
-//		return "Add new user";
-//	}
-	@RequestMapping("/getuser")
-//	public String getUser(@RequestParam String name) {
-//		return "Welcome, "+ name;
-//	}
-	public String getUser(@RequestParam(value="myname") String name) {
-		return "Welcome, "+ name + " to my Spring app ";
-	}
-	
 	// REST API development
-	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	@RequestMapping(value = "", method = RequestMethod.GET)
 	public List Users() {
 		Map user = new HashMap<>();
 		user.put("name", "Vinay");
