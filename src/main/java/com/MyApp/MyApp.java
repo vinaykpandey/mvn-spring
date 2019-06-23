2bbc5e2d9a772ee712c4941aaaf06128ebaae4d6
Vinay Pandey <vinay.pandey12@gmail.com>
2019-06-23 23:05:28 +0530

    read properties file

--- src/main/java/com/MyApp/MyApp.java
+++ src/main/java/com/MyApp/MyApp.java
@@ -1,5 +1,6 @@
 package com.MyApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,19 +9,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @Configuration
-@PropertySource("classpath:database.properties")
+@PropertySource({"classpath:database.properties", "classpath:email.properties"})
 public class MyApp {
 	public static void main(String []args) {
 //		SpringApplication.run(MyApp.class, args);
 		ConfigurableApplicationContext ctx = SpringApplication.run(MyApp.class, args);
+		
 		DataBase db = (DataBase)ctx.getBean(DataBase.class);
 		System.out.println("databse username: "+ db.getUsername());
 		System.out.println("databse password: "+ db.getPassword());
 		
+		Contact ct = (Contact)ctx.getBean(Contact.class);
+		System.out.println("Email address: "+ ct.getEmail());
+		System.out.println("Phone number: "+ ct.getPhone());
+		
 	}
+	@Autowired
+	Environment env;
 	
 	@Value("${mvapp.username}")
 	public String username;
@@ -28,12 +37,27 @@ public class MyApp {
 	@Value("${mvapp.password}")
 	public String password;
 	
+	@Value("${my_email}")
+	public String email;
+	
+	@Value("${my_phone}")
+	public String phone;
+	
 	@Bean
 	public DataBase getDetails() {
 		DataBase db = new DataBase();
+//		env_value = env.getProperty("ENV.NAME");
 		db.setUsername(username);
 		db.setPassword(password);
 		return db;
+	}
+	
+	@Bean
+	public Contact getContact() {
+		Contact ct = new Contact();
+		ct.setEmail(email);
+		ct.setPhone(phone);
+		return ct;
 	}
 	
 	@Bean
